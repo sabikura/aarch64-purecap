74b00464d1d67e9cd75810928fc5df9df6ba975b
Irina Nita <irina.nita@posteo.com>
2026-03-28 21:51:25 +0200

    examples: update fvp example to use entry macro
    Signed-off-by: Irina Nita <irina.nita@posteo.com>

--- examples/helloworld-fvp/src/main.rs
+++ examples/helloworld-fvp/src/main.rs
@@ -1,7 +1,13 @@
 #![no_std]
 #![no_main]
 
+use aarch64_purecap_rt::entry;
 mod pl011;
+
+#[entry]
+fn _main() -> ! {
+    loop {}
+}
 
 #[panic_handler]
 fn panic(_info: &core::panic::PanicInfo) -> ! {
