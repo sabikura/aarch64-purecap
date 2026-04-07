ecab5abb8cd1d501116f157a6cecfd0e7a2feca5
Irina Nita <irina.nita@posteo.com>
2026-04-08 00:59:30 +0300

    wip: figuring out setting up the exception table and stack pointers
    Signed-off-by: Irina Nita <irina.nita@posteo.com>

--- link.x
+++ link.x
@@ -2,13 +2,38 @@ MEMORY {
     ram (rwx) : ORIGIN = 0x80000000, LENGTH = 0x7f000000
 }
 
+__el1_stack_size = 0x10000;
+
 /* TODO: improve this */
-ENTRY(_entry_el2);
+ENTRY(_el2_drop_to_el1);
 EXTERN(_stack_setup);
 EXTERN(_init_stack);
 
 SECTIONS {
     .text : {
-        KEEP(*(.text.*))
+        KEEP(*(.text._el2_drop_to_el1))
+        __el1_entry_start = .;
+        KEEP(*(.text._el1_entry))
 	} >ram
+
+    .rodata : {
+        *(.rodata*)
+    } >ram
+
+    .data : {
+        __el1_data_start = .;
+        *(.data*)
+        __el1_data_end = .;
+    } >ram
+
+    .bss (NOLOAD) : {
+        __el1_bss_start = .;
+        *(.bss*)
+        *(COMMON)
+        __el1_bss_end = .;
+    } >ram
+
+    __el1_stack_end = .;
+    . = . + __el1_stack_size;
+    __el1_stack_start = .;
 }
--- src/lib.rs
+++ src/lib.rs
@@ -16,13 +16,14 @@ core::arch::global_asm!(
 // EL2 entry code (it sets up the system registers and then jumps to EL1)
 core::arch::global_asm!(
     r#"
-    .section .text._entry_el2
-    .global _entry_el2
-    .type _entry_el2, %function
-    _entry_el2:
+    .section .text._el2_drop_to_el1
+    .global _el2_drop_to_el1 
+    .type _el2_drop_to_el1, %function
+    _el2_drop_to_el1:
         // Disable traps for co-processor access
         // ARM DDI 0487; D24.2.77
         msr HSTR_EL2, xzr
+
         // Disable traps for advanced functionalities like FP or CHERI
         // ARM DDI 0606; 3.2.11
         mrs x0, CPTR_EL2
@@ -35,19 +36,22 @@ core::arch::global_asm!(
         orr x0, x0, #((1 << 19) | (1 << 18)) // CEN
         orr x0, x0, #((1 << 17) | (1 << 16)) // ZEN
         msr CPTR_EL2, x0
+
         // Don't know if this will bite later, but allow EL1 to execute
         // privileged capability creating instructions
         // ARM DDI 0606; 3.2.7
         mrs x0, CHCR_EL2
         bic x0, x0, 1 // SETTAG
         msr CHCR_EL2, x0
-        // More configuration..
+
+        // More configuration
         // NOTE: This is a BIG register, might need to look at each bit in the future (?)
         // ARM DDI 0487; D24.2.62
         mrs x0, HCR_EL2
         bic x0, x0, #(1 << 13) // TWI: Don't trap WFI instructions
         orr x0, x0, #(1 << 31) // RW:  EL1 runs in Aarch64
         msr HCR_EL2, x0
+
         // Configure access to timers
         // ARM DDI 0487; D24.10.2
         mrs x0, CNTHCTL_EL2
@@ -55,27 +59,37 @@ core::arch::global_asm!(
         msr CNTHCTL_EL2, x0 
         // ARM DDI 0487; D24.10.30
         msr CNTVOFF_EL2, xzr // Virtual offset for the timer is 0
+
         // TODO: Configure SCTLR_EL1 (not understanding the fields yet)
+
         // Configure the status register
         // ARM DDI 0606; 3.2.41
         mov x0, #0x3C5          // Mask D,A,I,F exceptions and return to EL1h stack pointer
         orr x0, x0, #(1 << 26)  // EL1 in full capability mode (C64)
         msr SPSR_EL2, x0
-        // Configure the Default Data Capability (DDC) for EL1 to full system access
-        mrs c0, DDC_EL2
-        msr DDC_EL1, c0
 
-        // TODO: Configure entry point for EL1 (CELR_EL2)
-        // ldr x0, =el1_entry
-        // scvalue c1, c0, x0          // derive cap from DDC
-        // msr CELR_EL2, c1
+        // Configure the Default Data Capability (DDC) for EL1 to full system access
+        // NOTE: I think this is useless;
+        //       ddc from all levels seem to have the same reset value
+        // mrs c0, DDC_EL2
+        // msr DDC_EL1, c0
+
+        // Configure the entry point capability for EL1
+        // ARM DDI 0606; 3.2.22
+        ldr x0, =__el1_entry_start
+        scvalue c1, ddc, x0
+        msr CELR_EL2, c1
 
         // TODO: Configure stack for EL1 (CSP_EL1)
-        // ldr x0, =el1_stack_top
-        // scvalue c1, c0, x0
-        // ldr x2, =el1_stack_size
-        // scbnds c1, c1, x2
-        // msr CSP_EL1, c1
+        // TODO: Edit PCC
+        ldr x0, =__el1_stack_start
+        scvalue c1, ddc, x0
+        ldr x2, =__el1_stack_size
+        // The bounds for c1 (base=ddc_base(0),length=ddc_length(full address space) should get
+        // translated to (base=__el1_stack_start, length=ddc_length)
+        // NOTE: Should this be done in EL1 and just change the address here?
+        scbnds c1, c1, x2
+        msr CSP_EL1, c1
 
         // TODO: Configure EL1 vector table (CVBAR_EL1)
         // 
